92783905702b1271b3d18e04f2eab679b2d6fa43
Palyasolo <soloduhin.pavel@ya.ru>
2024-07-13 15:50:51 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 1.2, "word", True
+print(immutable_var)
+#immutable_var [0] = 2 кортеж не предполагает изменения входящих в него данных
+mutable_list = [1, 1.2, "word", True]
+mutable_list [0] = 2
+print(mutable_list)
